02e153b4655e949d07f05ce9b23331e17a608c55
Olivverde <dele19270@uvg.edu.gt>
2022-09-19 15:03:00 -0600

    Outter analysis Completed

--- main.py
+++ main.py
@@ -18,9 +18,37 @@ class main(object):
         # Classes
         R = Reader(csvDoc)
         self.df = R.bundesLiga
+        #print(self.df)
+    # Entrada de eventos por video
+    def entry_amount_per_video(self):
+        df = self.df
+        df = df.groupby('video_id').size().to_frame('count')
+        #print(df.mean())
+        """
+        df = df.sort_values('count', ascending=False)
+        ax = df.plot.bar(y = 'count', use_index=True)
+        plt.title('Entries amount per video')
+        plt.ylabel('Amount of entries')
+        plt.xlabel('Video ID')
+        plt.show()
+        """
+    # Entrada de eventos por video
+    def event_amount_per_video(self):
+        df = self.df
+        df = self.df[df['event'] == 'end']
+        df = df[['event', 'video_id']]
+        df = df.groupby('video_id').count()
+        #print(df.mean())
+        """
+        df = df.sort_values('event', ascending=False)
+        ax = df.plot.bar(use_index=True)
+        plt.title('Completed events per video')
+        plt.ylabel('Amount of completed events')
+        plt.xlabel('Video ID')
+        plt.show()
+        """
 
-  
 
 exp = main('./data/train.csv')
-# exp.Pricing_Boxplot()
+exp.event_amount_per_video()
 
